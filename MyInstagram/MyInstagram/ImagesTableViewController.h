2ee3ddd2483123cc9e64fce86076d4e9c12d4917
Xoi Hin <xoihin@yahoo.com>
2015-11-05 12:27:48 -0500

    Checkpoint completed

---
+++ MyInstagram/MyInstagram/ImagesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImagesTableViewController.h
+//  MyInstagram
+//
+//  Created by Xoi's iMac on 2015-11-05.
+//  Copyright © 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+
+@end
