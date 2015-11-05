d851cfe007f273d3d90e852a8fbd591d7132ed98
Xoi Hin <xoihin@yahoo.com>
2015-11-05 10:22:16 -0500

    Initial Blocstagram commit

---
+++ MyInstagram/MyInstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyInstagram
+//
+//  Created by Xoi's iMac on 2015-11-05.
+//  Copyright © 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
